04c30ee21650969d8e34c85995557db69cc438f7
Andrey.Belov <andrey.belov.contact@gmail.com>
2020-10-06 20:38:14 +0300

    del: uuid

--- setup.py
+++ setup.py
@@ -1477,20 +1477,6 @@ class PyBuildExt(build_ext):
             )
         )
 
-    def detect_uuid(self):
-        # Build the _uuid module if possible
-        uuid_incs = find_file("uuid.h", self.inc_dirs, ["/usr/include/uuid"])
-        if uuid_incs is not None:
-            if self.compiler.find_library_file(self.lib_dirs, 'uuid'):
-                uuid_libs = ['uuid']
-            else:
-                uuid_libs = []
-            self.add(
-                Extension('_uuid', ['_uuidmodule.c'], libraries=uuid_libs, include_dirs=uuid_incs)
-            )
-        else:
-            self.missing.append('_uuid')
-
     def detect_modules(self):
         self.configure_compiler()
         self.init_inc_lib_dirs()
@@ -1509,7 +1495,6 @@ class PyBuildExt(build_ext):
         self.detect_decimal()
         self.detect_ctypes()
         self.detect_multiprocessing()
-        self.detect_uuid()
 
         ##         # Uncomment these lines if you want to play with xxmodule.c
         ##         self.add(Extension('xx', ['xxmodule.c']))
